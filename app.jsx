7ffd2b3a58dcba553b75ff35793e7662f6800e8a
eqdang <elizabethqdang@gmail.com>
2020-03-09 14:31:39 -0700

    initial commit

---
+++ app.jsx
@@ -0,0 +1,14 @@
+// import Game from './components/game';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+document.addEventListener("DOMContentLoaded", () =>
+{
+	ReactDOM.render(
+		// <Game />,
+		<div>
+			testing 123
+		</div>,
+		document.getElementById('main')
+	);
+});
